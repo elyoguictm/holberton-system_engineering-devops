66f56af821c5cd18c2d2367405a75679bfb908c7
elyoguictm <2788@holbertonschool.com>
2021-08-13 17:49:23 -0700

    add

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file
-file { 'holberton':
-     ensure  => file,
-     content => 'I love Puppet',
-     owner   => 'www-data',
-     group   => 'www-data',
-     mode    => '0744',
+file {'tmp/holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
