c2f83beadf7b243b77b340c9c65a328dc7998723
elyoguictm <2788@holbertonschool.com>
2021-08-14 16:11:42 -0700

    add

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# Connect to a server
+file_line {'Pass Auth':
+    path  => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/holberton',
+    match  => 'IdentityFile ~/.ssh/id_rsa',
+}
+file_line { 'no pass':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+}
