ffc8a6f8abd53376381e9203d8c20387411daaad
elyoguictm <2788@holbertonschool.com>
2021-09-13 17:33:42 -0700

    a

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-"""Script that returns info about employee ID"""
+"""
+Write a Python script that, using this REST API
+for a given employee ID, returns
+information about his/her TODO list progress.
+"""
 import requests
 from sys import argv
 
 
 if __name__ == '__main__':
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
-    alll = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1])).json()
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format
+                        (argv[1])).json()
+    alll = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                        .format(argv[1])).json()
     tasks = []
     name = user.get('name')
     for task in alll:
