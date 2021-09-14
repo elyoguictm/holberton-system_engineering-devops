56353d6ac4a05901a5c955557b1c27831b8f0679
elyoguictm <2788@holbertonschool.com>
2021-09-13 18:05:30 -0700

    ad

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,8 +4,10 @@ Write a Python script that, using this REST API
 for a given employee ID, returns
 information about his/her TODO list progress.
 """
+
 import requests
 from sys import argv
+
 if __name__ == '__main__':
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format
                         (argv[1])).json()
