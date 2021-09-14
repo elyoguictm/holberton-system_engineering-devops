8c3fc4c8c324e3c5af3d2283469ce6b442ba3578
elyoguictm <2788@holbertonschool.com>
2021-09-13 18:07:31 -0700

    a

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,9 @@
+
 #!/usr/bin/python3
 """
 Write a Python script that, using this REST API
 for a given employee ID, returns
-information about his/her TODO list progress.
+information about his/her list progress.
 """
 
 import requests
