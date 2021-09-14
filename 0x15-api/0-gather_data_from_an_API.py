b19702c39346905397426aac3d0692c399f24bde
elyoguictm <2788@holbertonschool.com>
2021-09-13 17:35:11 -0700

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,8 +6,6 @@ information about his/her TODO list progress.
 """
 import requests
 from sys import argv
-
-
 if __name__ == '__main__':
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format
                         (argv[1])).json()
