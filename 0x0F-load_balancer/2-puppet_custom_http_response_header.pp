25165ad078516413068248b3efb0f562ae9e30e3
elyoguictm <2788@holbertonschool.com>
2021-10-28 14:45:51 -0700

    pupp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,18 @@
+# create custom HTTP header
+
+exec {'/usr/bin/apt-get update':
+}
+
+package {'nginx':
+  ensure  => installed,
+}
+
+file_line {'configure custom header':
+  path => '/etc/nginx/sites-available/default',
+  after => 'server_name _;',
+  line => "\n\tadd_header X-Served-By \$hostname;\n",
+}
+
+service {'nginx':
+  ensure  => running,
+}
