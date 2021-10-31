6e216c353a14867901189c9ae0c1cfab9786575e
elyoguictm <2788@holbertonschool.com>
2021-10-30 19:49:49 -0700

    fixpup

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,26 @@
+# install and configure an Nginx server
+
+package { 'nginx':
+    ensure => present,
+}
+
+file { '/var/www/html/index.html':
+    ensure  => present,
+    path    => '/var/www/html/index.html',
+    content => 'Holberton School',
+}
+
+file_line {'Adding_redirect':
+    ensure => present,
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'server_name _;',
+    line   =>  '        rewrite ^/redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['Adding_redirect'],
+}
